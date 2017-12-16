895d2b69a915d2d11f9d504aa23e0436832add33
wk-j <somnuk.wk@gmail.com>
2017-12-16 23:06:19 +0700

    Fixed asset path

--- dotnet-format.rb
+++ dotnet-format.rb
@@ -1,7 +1,7 @@
 class DotnetFormat < Formula
   desc "Format C# document using Roslyn"
   homepage "https://github.com/wk-j/dotnet-format"
-  url "https://github.com/wk-j/dotnet-format/releases/download/0.1.0/dotnet-format.zip"
+  url "https://github.com/wk-j/dotnet-format/releases/download/0.1.0/dotnet-format.0.1.0.zip"
   version "0.1.0"
   sha256 "279f9e151cf39a7f5bc35e814340bb7477c102cd2d28bba7af650a26ec2b99c0"
 
